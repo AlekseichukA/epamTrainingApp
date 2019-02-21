6860faf5fd5717aba34f2e8887970923d5766cfe
AlekseichukA <alekseichuk4@gmail.com>
2019-02-21 16:02:21 +0300

    first changes

--- app/src/main/java/com/example/aaa/epamTraining/MainActivity.java
+++ app/src/main/java/com/example/aaa/epamTraining/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.aaa.epamTraining;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText searchField;
+    private Button button;
+    private TextView result;
+    //kgkgkjgkjgkjgj
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchField = findViewById(R.id.et_search_field);
+        button = findViewById(R.id.b_searh_vk);
+        result = findViewById(R.id.tv_result);
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText("кнопка была нажата");
+            }
+        };
+
+        button.setOnClickListener(onClickListener);
     }
 }
