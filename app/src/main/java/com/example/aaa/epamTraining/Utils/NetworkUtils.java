3e4351cd8d7aa7ac87c898e5b9cb8287bdac7a50
AlekseichukA <alekseichuk4@gmail.com>
2019-02-22 11:42:40 +0300

    I created new branch

--- app/src/main/java/com/example/aaa/epamTraining/Utils/NetworkUtils.java
+++ app/src/main/java/com/example/aaa/epamTraining/Utils/NetworkUtils.java
@@ -6,10 +6,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class NetworkUtils {
-    private static final String VK_API_BASIC_USER_URL = "https://api.vk.com/";
+    private static final String VK_API_BASIC_USER_URL = "https://api.vk.com";
     private static final String VK_USER_GET  = "/method/user.get";
     private static final String PARAM_USER_ID  = "user_id";
     private static final String PARAM_VERTION  = "v";
+
 
     public static URL generateURL(String userId) {
         Uri builtUri = Uri.parse(VK_API_BASIC_USER_URL + VK_USER_GET)
@@ -23,7 +24,10 @@ public class NetworkUtils {
         } catch (MalformedURLException ex) {
             ex.printStackTrace();
         }
-
         return url;
+    }
+
+    public static String getResponseFromURL(URL url) {
+
     }
 }
