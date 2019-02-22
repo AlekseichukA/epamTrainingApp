b0ed7ea0b6f5027173ba35860e6128fb8e3fbae7
AlekseichukA <alekseichuk4@gmail.com>
2019-02-22 12:17:49 +0300

    I made new branches

--- app/src/main/java/com/example/aaa/epamTraining/Utils/NetworkUtils.java
+++ app/src/main/java/com/example/aaa/epamTraining/Utils/NetworkUtils.java
@@ -17,6 +17,7 @@ public class NetworkUtils {
 
 
 
+
     public static URL generateURL(String userId) {
         Uri builtUri = Uri.parse(VK_API_BASIC_USER_URL + VK_USER_GET)
                 .buildUpon()
